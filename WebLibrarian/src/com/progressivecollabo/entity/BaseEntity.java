e37b5a9a1ffbddda78e971e757e6d5c7f230598a
Temitayo Odesanmi <roomtek@gmail.com>
2018-08-30 21:22:53 -0400

    Added Classes

--- WebLibrarian/src/com/progressivecollabo/entity/BaseEntity.java
+++ WebLibrarian/src/com/progressivecollabo/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.progressivecollabo.entity;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Base class for all persistable data
@@ -9,8 +10,34 @@ import java.time.LocalDateTime;
 public class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	String uuid; // 3PUB-YEAR-UUID
 
+	String craetedBy;
 	LocalDateTime createdDate;
+	String modifiedBy;
 	LocalDateTime modifiedDate;
-
+	String archivedBy;
+	LocalDateTime archivedDate;
+	
+	boolean archived;
+	
+	List<Pricing> pricinginformation;
+	
+	String language;
+	String publisherID;
+	String publisherName;
+	String releaseYear;
+	String publisherWebsite; 
+	String copyright; 
+	
+	public static class Pricing {
+		String priceType; // RENTAL, BUY
+		double unitCost;
+		String unitCostCurrency;
+	}
+	
+	// 0 - true, 12.3 $
+	// 1 - false, 24.4 $
+	
 }
