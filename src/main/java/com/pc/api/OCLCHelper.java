4bfcb9c9c8201a44c40391ae593967a3a1fbc68c
roomtek <roomtek@gmail.com>
2018-10-20 20:56:28 -0400

    Update src/main/java/com/pc/api/OCLCHelper.java
    Added OCLC

---
+++ src/main/java/com/pc/api/OCLCHelper.java
@@ -0,0 +1,74 @@
+package com.pc.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class OCLCHelper {
+
+	private final static String baseUrl = "http://classify.oclc.org/classify2";
+
+	public static void main(String... args) {
+		List<OCLCWork> works = findByISBN("9781612680057");
+
+		for (OCLCWork oclcWork : works) {
+			System.out.println(oclcWork);
+		}
+	}
+
+	static List<OCLCWork> findByISBN(String isbn) {
+		try {
+			Document doc = Jsoup.connect(baseUrl + "/Classify?").data("isbn", isbn, "summary", "true").get();
+
+			Elements works = doc.select("work");
+
+			List<OCLCWork> publ = new ArrayList<>();
+			for (Element element : works) {
+				OCLCWork p = new OCLCWork();
+				p.author = element.attr("author");
+				p.format = element.attr("format");
+				p.hyear = element.attr("hyr");
+				p.lyear = element.attr("lyr");
+				p.owi = element.attr("owi");
+				p.title = element.attr("title");
+				p.wi = element.attr("wi");
+				p.editions = element.attr("editions");
+				p.holdings = element.attr("holdings");
+				p.itemtype = element.attr("itemtype");
+				publ.add(p);
+			}
+			return publ;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+	public static class OCLCWork {
+		public String author;
+		public String format;
+		public String hyear;
+		public String lyear;
+		public String owi;
+		public String title;
+		public String wi;
+		public String editions;
+		public String holdings;
+		public String itemtype;
+
+		@Override
+		public String toString() {
+			return "OCLCWork [author=" + author + ", format=" + format + ", hyear=" + hyear + ", lyear=" + lyear
+					+ ", owi=" + owi + ", title=" + title + ", wi=" + wi + ", editions=" + editions + ", holdings="
+					+ holdings + ", itemtype=" + itemtype + "]";
+		}
+
+	}
+
+}
